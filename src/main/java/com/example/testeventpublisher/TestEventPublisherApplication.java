a506dbde39aa32589b857d2ef7221c17d198328e
otacu <otacu@sina.com>
2022-06-22 10:00:30 +0800

    Update TestEventPublisherApplication.java
    异步注解@Async要加@EnableAsync

--- src/main/java/com/example/testeventpublisher/TestEventPublisherApplication.java
+++ src/main/java/com/example/testeventpublisher/TestEventPublisherApplication.java
@@ -3,7 +3,9 @@ package com.example.testeventpublisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class TestEventPublisherApplication {
 
